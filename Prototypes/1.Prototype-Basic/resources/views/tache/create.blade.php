c8ebd1065ead8172fc77e6d0482bed0121a319c7
stitou-nada <nadastito1087@gmail.com>
2022-11-08 14:47:10 +0100

    style

--- Prototypes/1.Prototype-Basic/resources/views/tache/create.blade.php
+++ Prototypes/1.Prototype-Basic/resources/views/tache/create.blade.php
@@ -7,10 +7,10 @@
         Nom du tache :<input type="text" class="form-control"name="nom" >
       </div>
       <div class="form-group col-md-2">
-        Début de la tache:<input type="datetime-local" class="form-control"  name="livraison" >
+        Début de la tache:<input type="datetime-local" class="form-control"  name="debut_tache" >
       </div>
       <div class="form-group col-md-2">
-        Fin de la tache:<input type="datetime-local" class="form-control" name="recuperation" >
+        Fin de la tache:<input type="datetime-local" class="form-control" name="fin_tache" >
       </div>
       <input type="hidden" value="{{$id}}" name="brief_id">
 
--- Prototypes/1.Prototype-Basic/resources/views/tache/edit.blade.php
+++ Prototypes/1.Prototype-Basic/resources/views/tache/edit.blade.php
@@ -1,19 +1,6 @@
 @include('layouts.head')
 
 @foreach ($tache as $value)
-<form  action="{{route('tache.update',$value->id)}}" method="POST" >
-    @csrf
-    @method("PUT")
-    nom du brief : <input type="text" value="{{$value->Nom_de_la_tache}}" name="nom">
-    Date/heure de livraison : <input type="datetime-local" value="{{$value->Debut_de_la_tache}}" name="debut_tache">
-    Date/heure de récupération : <input type="datetime-local"  value="{{$value->Fin_de_la_tache}}" name="fin_tache">
-    <input type="hidden" value="{{$value->brief_id}}" name="brief_id">
-    <button type="submit">Editer</button>
-</form>
-@endforeach
-
-
-
 <form  action="{{route('tache.update',$value->id)}}" method="POST">
     @csrf
     @method("PUT")
@@ -23,10 +10,10 @@
         Nom du tache :<input type="text" class="form-control" value="{{$value->Nom_de_la_tache}}" name="nom" >
       </div>
       <div class="form-group col-md-2">
-        Début de la tache:<input type="datetime-local" class="form-control" value="{{$value->Debut_de_la_tache}}" name="livraison" >
+        Début de la tache:<input type="datetime-local" class="form-control" value="{{$value->Debut_de_la_tache}}" name="debut_tache" >
       </div>
       <div class="form-group col-md-2">
-        Fin de la tache:<input type="datetime-local" class="form-control" value="{{$value->Fin_de_la_tache}}" name="recuperation" >
+        Fin de la tache:<input type="datetime-local" class="form-control" value="{{$value->Fin_de_la_tache}}" name="fin_tache" >
       </div>
       <input type="hidden" value="{{$value->brief_id}}" name="brief_id">
 
@@ -34,3 +21,4 @@
 
     <button type="submit" class="btn btn-warning">Editer</button>
   </form>
+  @endforeach
