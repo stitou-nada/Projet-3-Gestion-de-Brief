04d1ad8c25886fca1f4ef3fe889546a1b2be9f37
stitou-nada <nadastito1087@gmail.com>
2022-11-09 10:06:04 +0100

    migration

--- Prototypes/3.Prototype-premium/database/migrations/2021_11_09_085155_promotion.php
+++ Prototypes/3.Prototype-premium/database/migrations/2021_11_09_085155_promotion.php
@@ -13,14 +13,10 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
-            $table->timestamps();
+        Schema::create('promotion', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string("nom")->nullable();
+
         });
     }
 
@@ -31,6 +27,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        //
     }
 };
--- Prototypes/3.Prototype-premium/database/migrations/2022_11_06_165204_create_briefs_table.php
+++ Prototypes/3.Prototype-premium/database/migrations/2022_11_06_165204_create_briefs_table.php
@@ -13,14 +13,11 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('failed_jobs', function (Blueprint $table) {
-            $table->id();
-            $table->string('uuid')->unique();
-            $table->text('connection');
-            $table->text('queue');
-            $table->longText('payload');
-            $table->longText('exception');
-            $table->timestamp('failed_at')->useCurrent();
+        Schema::create('briefs', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string("Nom_du_brief")->nullable();
+            $table->timestamp("Date_heure_de_livraison")->nullable();
+            $table->timestamp("Date_heure_de_récupération")->nullable();
         });
     }
 
@@ -31,6 +28,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('failed_jobs');
+        Schema::dropIfExists('briefs');
     }
 };
